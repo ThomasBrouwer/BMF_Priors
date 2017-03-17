239a6cd710e2c28a13b1dbb596d99d639ebd88de
ThomasBrouwer <tab43@cam.ac.uk>
2017-03-17 14:46:32 +0000

    Finished parameter value updates for all models.

--- code/models/Gibbs/parameters.py
+++ code/models/Gibbs/parameters.py
@@ -46,25 +46,57 @@ def poisson_zij_n_p(Rij, Ui, Vj):
 
 
 ''' (Gausian) Gaussian '''
-def gaussian_gaussian_mu_sigma():
-    #TODO:
-    pass
+def gaussian_gaussian_mu_sigma(lamb, Ri, Mi, V, tau):
+    """ mu and sigma for Ui with N(0,I/lamb) prior (I=identity matrix). """
+    assert Ri.shape == Mi.shape and Ri.shape == V.shape[0]
+    K = V.shape[1]
+    V_masked = (Mi * V.T).T # zero rows when j not in Mi
+    precision = lamb * numpy.eye(K) + tau * ( numpy.dot(V_masked.T,V_masked) )
+    sigma = numpy.linalg.inv(precision)
+    Ri_masked = Mi * Ri # zero entries when j not in Mi
+    mu = numpy.dot(sigma, tau * numpy.dot(Ri_masked, V))
+    assert mu.shape[0] == V.shape[1] and sigma.shape == (V.shape[1], V.shape[1])
+    return (mu, sigma)
 
 
 ''' (Gausian) Gaussian + Wishart '''
-def gaussian_gaussian_wishart_mu_sigma():
-    #TODO:
-    pass
+def gaussian_gaussian_wishart_mu_sigma(muU, sigmaU_inv, Ri, Mi, V, tau):
+    """ mu and sigma for Ui with N(muU,sigmaU) prior. """
+    assert Ri.shape == Mi.shape and Ri.shape == V.shape[0]
+    V_masked = (Mi * V.T).T # zero rows when j not in Mi
+    precision = sigmaU_inv + tau * ( numpy.dot(V_masked.T,V_masked) )
+    sigma = numpy.linalg.inv(precision)
+    Ri_masked = Mi * Ri # zero entries when j not in Mi
+    mu = numpy.dot(sigma, numpy.dot(sigmaU_inv, muU) + tau * numpy.dot(Ri_masked, V))
+    assert mu.shape[0] == V.shape[1] and sigma.shape == (V.shape[1], V.shape[1])
+    return (mu, sigma)
 
-def gaussian_wishart_beta0_v0_mu0_W0():
-    #TODO:
-    pass
+def gaussian_wishart_beta0_v0_mu0_W0(beta0, v0, mu0, W0_inv, U):
+    """ beta0_s, v0_s, mu0_s, W0_s for muU, sigmaU with NIW(beta0,v0,mu0,W0) prior. """
+    I, K = U.shape
+    assert mu0.shape == (K,) and W0_inv.shape == (K, K)
+    beta0_s = beta0 + I
+    v0_s = v0 + I
+    U_bar = U.sum(axis=0) / float(I) # vector giving average per column of U
+    S_bar = numpy.dot(U.T, U) # matrix giving covariance of columns of U
+    mu0_s = ( beta0 * mu0 + I * U_bar) / ( beta0 + I )
+    W0_s_inv = W0_inv + I * S_bar + (beta0*I)/(beta0+I) * numpy.outer(mu0-U_bar, mu0-U_bar)
+    W0_s = numpy.linalg.inv(W0_s_inv)
+    assert mu0_s.shape == (K,) and W0_s.shape == (K, K)
+    return (beta0_s, v0_s, mu0_s, W0_s)
 
 
 ''' (Gausian) Gaussian + Automatic Relevance Determination '''
-def gaussian_gaussian_ard_mu_sigma():
-    #TODO:
-    pass
+def gaussian_gaussian_ard_mu_sigma(lamb, Ri, Mi, V, tau):
+    """ mu and sigma for Ui with N(0,diag(1/lamb)) prior. lamb is a vector. """
+    assert Ri.shape == Mi.shape and Ri.shape == V.shape[0] and lamb.shape[0] == V.shape[1]
+    V_masked = (Mi * V.T).T # zero rows when j not in Mi
+    precision = numpy.diag(lamb) + tau * ( numpy.dot(V_masked.T,V_masked) )
+    sigma = numpy.linalg.inv(precision)
+    Ri_masked = Mi * Ri # zero entries when j not in Mi
+    mu = numpy.dot(sigma, tau * numpy.dot(Ri_masked, V))
+    assert mu.shape[0] == V.shape[1] and sigma.shape == (V.shape[1], V.shape[1])
+    return (mu, sigma)
 
 def gaussian_ard_alpha_beta(alpha0, beta0, Uk, Vk):
     """ alpha_s and beta_s for lambdak with Gamma(alpha0,beta0) prior. """
@@ -74,14 +106,35 @@ def gaussian_ard_alpha_beta(alpha0, beta0, Uk, Vk):
     return (alpha_s, beta_s)
 
 ''' (Gausian) Gaussian + Volume Prior '''
-def gaussian_gaussian_vp_mu_sigma():
-    #TODO:
-    pass
+def adjugate_matrix(matrix):
+    """ adj(matrix) = det(matrix) matrix^-1 """
+    return numpy.linalg.det(matrix) * numpy.linalg.inv(matrix)
+
+def gaussian_gaussian_vp_mu_sigma(i, k, gamma, Ri, Mi, U, V, tau):
+    """ muUik and tauUik for Uik with Volume Prior, exp{-gamma det(U.T U)}. """
+    I, K, J = U.shape[0], U.shape[1], Ri.shape[0]
+    assert Mi.shape == (J,) and V.shape == (J, K)
+    U_i_ktilde = numpy.append(U[i,:k],U[i,k+1:]) # vector Ui excl entry k
+    U_itilde_k = numpy.append(U[:i,k],U[i+1:,k]) # vector Uk excl entry i
+    U_ktilde = numpy.append(U[:,:k],U[:,k+1:],axis=1) # matrix U excl column k
+    U_itilde_ktilde = numpy.append(U_ktilde[:i,:],U_ktilde[i+1:,:],axis=0) # matrix U excl row i and column k
+    assert U_i_ktilde.shape == (K-1,) and U_itilde_k.shape == (I-1,)
+    assert U_ktilde.shape == (I,K-1) and U_itilde_ktilde.shape == (I-1,K-1)
+    cov_U_ktilde = numpy.dot(U_ktilde.T, U_ktilde)
+    D_ktilde_ktilde = numpy.linalg.det(cov_U_ktilde)
+    A_ktilde_ktilde = adjugate_matrix(cov_U_ktilde)
+    assert cov_U_ktilde.shape == (K-1,K-1) and A_ktilde_ktilde.shape == (K-1,K-1)
+    tauUik = tau*(Mi*V[:,k]**2).sum() + gamma * (D_ktilde_ktilde - numpy.dot(numpy.dot(U_i_ktilde,A_ktilde_ktilde),U_i_ktilde))
+    muUik = 1./tauUik * (
+        tau * (Mi *((Ri-numpy.dot(U[i,:],V.T)+U[i,k]*V[:,k])*V[:,k])).sum() + 
+        gamma * numpy.dot(numpy.dot(U_i_ktilde,A_ktilde_ktilde), numpy.dot(U_itilde_ktilde.T,U_itilde_k))
+    )
+    return (muUik, tauUik)
 
 
 ''' (Gausian) Exponential '''
 def gaussian_exponential_mu_tau(k, lamb, R, M, U, V, tau):
-    """ mu and tau for Uik with Exp(lamb) prior. 
+    """ muUik and tauUik for Uik with Exp(lamb) prior. 
         We do updates per column of U (so Uk). """
     assert R.shape == M.shape and R.shape[0] == U.shape[0] and R.shape[1] == V.shape[0]
     assert U.shape[1] == V.shape[1]
