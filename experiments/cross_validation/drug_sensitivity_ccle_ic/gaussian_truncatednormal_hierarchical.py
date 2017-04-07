1dc130524bd058193a11072d31eb67ff65de6187
ThomasBrouwer <tab43@cam.ac.uk>
2017-04-07 10:27:21 +0200

    Ran crossval CCLE IC datasets.

---
+++ experiments/cross_validation/drug_sensitivity_ccle_ic/gaussian_truncatednormal_hierarchical.py
@@ -0,0 +1,57 @@
+'''
+Run nested cross-validation experiment on the CCLE IC drug sensitivity dataset, with 
+the Gaussian + Truncated Normal + hierarchical model.
+'''
+
+project_location = "/Users/thomasbrouwer/Documents/Projects/libraries/"
+import sys
+sys.path.append(project_location)
+
+from BMF_Priors.code.models.bmf_gaussian_truncatednormal_hierarchical import BMF_Gaussian_TruncatedNormal_Hierarchical
+from BMF_Priors.code.cross_validation.nested_matrix_cross_validation import MatrixNestedCrossValidation
+from BMF_Priors.data.drug_sensitivity.load_data import load_ccle_ic50_integer
+
+
+''' Settings BMF model. '''
+method = BMF_Gaussian_TruncatedNormal_Hierarchical
+R, M = load_ccle_ic50_integer()
+hyperparameters = { 'alpha':1., 'beta':1., 'mu_mu':0., 'tau_mu':0.1, 'a':1., 'b':1. }
+train_config = {
+    'iterations' : 200,
+    'init' : 'random',
+}
+predict_config = {
+    'burn_in' : 180,
+    'thinning' : 1,
+}
+
+
+''' Settings nested cross-validation. '''
+K_range = [1,2,3,4,5,6]
+no_folds = 5
+no_threads = 5
+stratify_rows = False
+parallel = False
+folder_results = './results/gaussian_truncatednormal_hierarchical/'
+output_file = folder_results+'results.txt'
+files_nested_performances = [folder_results+'fold_%s.txt'%(fold+1) for fold in range(no_folds)]
+
+
+''' Construct the parameter search. '''
+parameter_search = [{'K':K, 'hyperparameters':hyperparameters} for K in K_range]
+
+
+''' Run the cross-validation framework. '''
+nested_crossval = MatrixNestedCrossValidation(
+    method=method,
+    R=R,
+    M=M,
+    K=no_folds,
+    P=no_threads,
+    parameter_search=parameter_search,
+    train_config=train_config,
+    predict_config=predict_config,
+    file_performance=output_file,
+    files_nested_performances=files_nested_performances,
+)
+nested_crossval.run(parallel=parallel, stratify_rows=stratify_rows)
