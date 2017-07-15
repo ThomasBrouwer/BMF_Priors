bcd60967523e0343709a037e12484f5bce82be4b
ThomasBrouwer <tab43@cam.ac.uk>
2017-07-15 14:10:15 +0200

    Timing code efficiency on my laptop vs kiiara. Going to run it now.

--- experiments/time_code.py
+++ experiments/time_code.py
@@ -2,32 +2,37 @@
 Measure the time taken for a couple of iterations.
 '''
 
-project_location = "/Users/thomasbrouwer/Documents/Projects/libraries/" # "/home/tab43/Documents/Projects/libraries" # 
+project_location = "/home/tab43/Documents/Projects/libraries" # "/Users/thomasbrouwer/Documents/Projects/libraries/" # 
 import sys
 sys.path.append(project_location)
 
 from BMF_Priors.code.models.bmf_gaussian_gaussian import BMF_Gaussian_Gaussian
-from BMF_Priors.data.drug_sensitivity.load_data import load_gdsc_ic50_integer
+from BMF_Priors.data.movielens.load_data import load_movielens_1M
 
 import time
 
 
 ''' Load data and set settings. '''
-R, M = load_gdsc_ic50_integer()
 K = 10
 hyperparameters = { 'alpha':1., 'beta':1., 'lamb':0.1 }
-iterations = 20
+iterations = 10
 init = 'random'
+
+time0 = time.time()
+R, M = load_movielens_1M()
+time1 = time.time()
+time_data = time1 - time0
 
 
 ''' Run without minpy (so numpy). '''
-time0 = time.time()
+time2 = time.time()
 BMF = BMF_Gaussian_Gaussian(R,M,K,hyperparameters) 
 BMF.initialise(init)
 BMF.run(iterations)
-time1 = time.time()
-time = time1 - time0
+time3 = time.time()
+time_model = time3 - time2
 
 
 ''' Print performances. '''
-print "Time taken: %s." % time
+print "Time taken loading data: %s." % time_data
+print "Time taken running model: %s." % time_model
