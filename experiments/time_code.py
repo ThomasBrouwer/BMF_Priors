21e224980e75a56e3c47af34ff0b489e1d505422
ThomasBrouwer <tab43@cam.ac.uk>
2017-07-18 12:42:40 +0100

    Before merge

--- experiments/time_code.py
+++ experiments/time_code.py
@@ -15,14 +15,33 @@ On kiiara:
     MovieLens 1M:
         Time taken loading data: 101.879905939.
         Time taken running model: 119.432013988.
+        
+After making data loading more efficient:
+
+On my laptop:
+    MovieLens 100K:
+        Time taken loading data: 0.0656440258026.
+        Time taken running model: 8.53658103943.
+    MovieLens 1M:
+        Time taken loading data: 0.324141025543.
+        Time taken running model: 83.3057899475.
+On Darwin cluster:
+    MovieLens 100K:
+        Time taken loading data: 0.0200979709625.
+        Time taken running model: 10.8945901394.
+    MovieLens 1M:
+        Time taken loading data: 0.193010091782.
+        Time taken running model: 69.9377059937.
+    
 '''
 
-project_location = "/home/tab43/Documents/Projects/libraries" # "/Users/thomasbrouwer/Documents/Projects/libraries/" # 
+project_location = "/Users/thomasbrouwer/Documents/Projects/libraries/" # "/home/tab43/Documents/Projects/libraries" # 
 import sys
 sys.path.append(project_location)
 
 from BMF_Priors.code.models.bmf_gaussian_gaussian import BMF_Gaussian_Gaussian
-from BMF_Priors.data.movielens.load_data import load_movielens_1M
+from BMF_Priors.data.movielens.load_data import load_processed_movielens_1M
+from BMF_Priors.data.movielens.load_data import load_processed_movielens_100K
 
 import time
 
@@ -34,7 +53,7 @@ iterations = 10
 init = 'random'
 
 time0 = time.time()
-R, M = load_movielens_1M()
+R, M = load_processed_movielens_1M() # load_processed_movielens_100K() # 
 time1 = time.time()
 time_data = time1 - time0
 
