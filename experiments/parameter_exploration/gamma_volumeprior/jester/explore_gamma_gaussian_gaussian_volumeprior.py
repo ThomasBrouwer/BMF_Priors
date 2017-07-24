fe9b9fe7bac155271028f9598912db080792adc3
ThomasBrouwer <tab43@cam.ac.uk>
2017-07-24 10:31:54 +0100

    Set up gamma parameter search for Jester dataset.

--- experiments/parameter_exploration/gamma_volumeprior/jester/explore_gamma_gaussian_gaussian_volumeprior.py
+++ experiments/parameter_exploration/gamma_volumeprior/jester/explore_gamma_gaussian_gaussian_volumeprior.py
@@ -18,8 +18,8 @@ import matplotlib.pyplot as plt
 ''' Run the experiment for the Gaussian + Gaussian + VP model. '''
 R, M = load_processed_jester_data_integer()
 model_class = BMF_Gaussian_Gaussian_VolumePrior
-n_folds = 5
-values_gamma = [10**-40, 10**-30, 10**-20, 10**-10, 10**0, 10**10, 10**20] 
+n_folds = 3
+values_gamma = [10**-30, 10**-20, 10**-10, 10**0, 10**10] 
 values_K = [2, 5]
 values_gamma_K = list(itertools.product(values_gamma, values_K))
 settings = {
@@ -27,8 +27,8 @@ settings = {
     'M': M, 
     'hyperparameters': { 'alpha':1., 'beta':1., 'lamb':0.1 }, 
     'init': 'random', 
-    'iterations': 100,
-    'burn_in': 80,
+    'iterations': 70,
+    'burn_in': 50,
     'thinning': 1,
 }
 fout = './performances_gaussian_gaussian_volumeprior.txt'
