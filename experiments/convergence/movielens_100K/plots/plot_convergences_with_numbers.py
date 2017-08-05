cba0d9f94a0e4af6cc63628fcffc020012364f5d
ThomasBrouwer <tab43@cam.ac.uk>
2017-08-05 17:47:55 +0100

    Nearly finished running experiments for Laplace models. Updated plotting scripts quite a bit.

---
+++ experiments/convergence/movielens_100K/plots/plot_convergences_with_numbers.py
@@ -0,0 +1,84 @@
+"""
+Plot the convergence of the many different BMF algorithms on the MovieLens 100K data.
+"""
+
+import matplotlib.pyplot as plt
+
+
+''' Plot settings. '''
+MSE_min, MSE_max = 0.4, 1.2
+it_min, it_max = 0, 200
+iterations = range(1,200+1)
+
+folder_plots = "./"
+folder_results = "./../results/"
+plot_file = folder_plots+"convergences_movielens100k_numbered.png"
+
+
+''' Load in the performances. '''
+ggg = eval(open(folder_results+'performances_gaussian_gaussian.txt','r').read())
+gggu = eval(open(folder_results+'performances_gaussian_gaussian_univariate.txt','r').read())
+gggw = eval(open(folder_results+'performances_gaussian_gaussian_wishart.txt','r').read())
+ggga = eval(open(folder_results+'performances_gaussian_gaussian_ard.txt','r').read())
+gll = eval(open(folder_results+'performances_gaussian_laplace.txt','r').read())
+glli = eval(open(folder_results+'performances_gaussian_laplace_ig.txt','r').read())
+gvg = eval(open(folder_results+'performances_gaussian_gaussian_volumeprior.txt','r').read())
+gvng = eval(open(folder_results+'performances_gaussian_gaussian_volumeprior_nonnegative.txt','r').read())
+geg = eval(open(folder_results+'performances_gaussian_gaussian_exponential.txt','r').read())
+gee = eval(open(folder_results+'performances_gaussian_exponential.txt','r').read())
+geea = eval(open(folder_results+'performances_gaussian_exponential_ard.txt','r').read())
+gtt = eval(open(folder_results+'performances_gaussian_truncatednormal.txt','r').read())
+gttn = eval(open(folder_results+'performances_gaussian_truncatednormal_hierarchical.txt','r').read())
+gll = eval(open(folder_results+'performances_gaussian_l21.txt','r').read())
+pgg = eval(open(folder_results+'performances_poisson_gamma.txt','r').read())
+pggg = eval(open(folder_results+'performances_poisson_gamma_gamma.txt','r').read())
+
+nmf_np = eval(open(folder_results+'performances_baseline_mf_nonprobabilistic.txt').read())
+row = eval(open(folder_results+'performances_baseline_average_row.txt','r').read())
+column = eval(open(folder_results+'performances_baseline_average_column.txt','r').read())
+
+
+''' Assemble the average performances and method names. '''
+performances_names_colours_linestyles_markers = [
+    (ggg,  'GGG',  'r', '-', '1'),
+    (gggu, 'GGGU', 'r', '-', '2'),
+    (ggga, 'GGGA', 'r', '-', '3'),
+    (gggw, 'GGGW', 'r', '-', '4'),
+    (gll,  'GLL',  'r', '-', '5'),
+    (glli, 'GLLI', 'r', '-', '6'),
+    (gvg,  'GVG',  'r', '-', '7'),
+    (gee,  'GEE',  'b', '-', '1'),
+    (geea, 'GEEA', 'b', '-', '2'),
+    (gtt,  'GTT',  'b', '-', '3'),
+    (gttn, 'GTTN', 'b', '-', '4'),
+    (gll,  'GLL',  'b', '-', '5'),
+    (geg,  'GEG',  'g', '-', '1'),
+    (gvng, 'GVnG', 'g', '-', '2'),
+    (pgg,  'PGG',  'y', '-', '1'),
+    (pggg, 'PGGG', 'y', '-', '2'),
+    (nmf_np, 'Row',    'grey', '-', '1'),
+    (column, 'NMF-NP', 'grey', '-', '2'),
+    (row,    'Col',    'grey', '-', '3'),
+]
+
+
+''' Plot the performances. '''
+fig = plt.figure(figsize=(3,2.25))
+fig.subplots_adjust(left=0.125, right=0.97, bottom=0.135, top=0.97)
+plt.xlabel("Iterations", fontsize=9, labelpad=0)
+plt.ylabel("MSE", fontsize=9, labelpad=1)
+plt.yticks(fontsize=6)
+plt.xticks(fontsize=6)
+
+x = iterations
+for performances, name, colour, linestyle, marker in performances_names_colours_linestyles_markers:
+    y = performances[:iterations[-1]]
+    print name, performances[-1]
+    plt.plot(x, y, label=name, linestyle=linestyle, c=colour, marker=('$%s$' % marker), 
+             markevery=10, markersize=3, linewidth=0.5)
+ 
+plt.ylim(MSE_min,MSE_max)
+plt.xlim(it_min, it_max)
+    
+plt.savefig(plot_file, dpi=600)
+    
---
+++ experiments/factor_analysis/drug_sensitivity_gdsc/gaussian_laplace.py
@@ -0,0 +1,34 @@
+'''
+Run factor analysis experiment on the GDSC drug sensitivity dataset, with 
+the Gaussian + Laplace model.
+'''
+
+project_location = "/Users/thomasbrouwer/Documents/Projects/libraries/"
+import sys
+sys.path.append(project_location)
+
+from BMF_Priors.code.models.bmf_gaussian_laplace import BMF_Gaussian_Laplace
+from BMF_Priors.data.drug_sensitivity.load_data import load_gdsc_ic50_integer
+from BMF_Priors.experiments.factor_analysis.factor_analysis import run_model_store_matrices
+
+import math
+
+
+''' Run the experiment. '''
+R, M = load_gdsc_ic50_integer()
+model_class = BMF_Gaussian_Laplace
+n_repeats = 10
+settings = {
+    'R': R, 
+    'M': M, 
+    'K': 10,
+    'hyperparameters': { 'alpha':1., 'beta':1., 'eta':math.sqrt(10.) }, 
+    'init': 'random', 
+    'iterations': 300,
+    'burn_in': 200,
+    'thinning': 2,
+}
+fout_U = './results/gaussian_laplace_U.txt'
+fout_V = './results/gaussian_laplace_V.txt'
+all_expU, all_expV = run_model_store_matrices(
+    n_repeats=n_repeats, model_class=model_class, settings=settings, fout_U=fout_U, fout_V=fout_V)
